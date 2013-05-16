6bbb4a698078eb1b3e235f7ba0b293cace77763e
David Chawei Hsu <chawei.hsu@gmail.com>
2013-05-16 06:46:42 -0700

    add menu

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
+  this.route('multiview',        { path: "/multiview" })
+  this.route('comparison',       { path: "/comparison" })
+  this.route('item_association', { path: "/item_association" })
+  this.route('significance',     { path: "/significance" })
+  this.route('diagnosis',        { path: "/diagnosis" })
 });
 
 App.IndexRoute = Ember.Route.extend({
