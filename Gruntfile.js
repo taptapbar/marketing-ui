cf5dd264d1cdc3b97b82ae52018b4c8d854d06ed
David Chawei Hsu <chawei.hsu@gmail.com>
2013-05-16 14:12:23 -0700

    setup grunt and extract template scripts to external files

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+  // Do grunt-related things in here
+  grunt.initConfig({
+    ember_templates: {
+      compile: {
+        options: {
+          templateName: function(sourceFile) {
+            return sourceFile.replace(/templates\//, '');
+          }
+        },
+        files: {
+          "templates/templates.js": "templates/*.handlebars"
+        }
+      }
+    },
+    
+    sass: {
+      dist: {
+        files: {
+          'css/app.css': [
+            'css/main.scss'
+          ]
+        }
+      }
+    },
+  
+    watch: {
+      ember_templates: {
+        files: 'templates/**/*.handlebars',
+        tasks: ['ember_templates']
+      },
+      
+      sass: {
+        files: 'css/*.scss',
+        tasks: ['sass']
+      }
+    }
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-ember-templates');
+  grunt.registerTask('default', ['ember_templates', 'sass']);
+};
---
+++ templates/templates.js
@@ -0,0 +1,19 @@
+Ember.TEMPLATES["index"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+  
+
+
+  data.buffer.push("awesome");
+  
+});
+
+Ember.TEMPLATES["multiview"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [2,'>= 1.0.0-rc.3'];
+helpers = helpers || Ember.Handlebars.helpers; data = data || {};
+  
+
+
+  data.buffer.push("happy");
+  
+});
